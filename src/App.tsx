bca96cf6de826be9257cc9d02ba6e236f1a0e4dc
Rakesh-46-VR <rakeshbsirvi46@gmail.com>
2024-12-03 01:00:40 +0530

    fixes in roadmap , ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,91 @@
-import Navbar from "./components/navbar";
-import Footer from "./components/footer";
-import { ArrowRight, Lightbulb, IndianRupee, Users } from 'lucide-react';
-import Rules from "./components/rules";
-import TeaRoadmap from "./components/roadmap";
+import { useState, useEffect } from "react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import { ArrowRight, Lightbulb, IndianRupee, Users } from "lucide-react";
+import Rules from "./components/Rules";
+import RoadMap from "./components/RoadMap";
+import RoadMapMobile from "./components/RoadMapMobile";
 import Marquee from "@/components/ui/marquee";
 
 function App() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    handleResize(); // Set initial state
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className="min-h-screen bg-gradient-to-br from-purple-700 to-blue-500 text-white">
       <Navbar />
-      <main className="container mx-auto  md:py-20">
+      <main className="container mx-auto px-4 py-10 md:py-20">
         <section className="text-center mb-10 md:mb-20">
-          <h1 className="text-4xl md:text-6xl font-bold mb-6 animate-fade-in-down">
+          <h1 className="text-3xl md:text-6xl font-bold mb-4 md:mb-6 animate-fade-in-down">
             Idea Challenge Competition
           </h1>
-          <p className="text-xl md:text-3xl mb-8 animate-fade-in-up">
+          <p className="text-lg md:text-3xl mb-6 md:mb-8 animate-fade-in-up">
             Pitch Your Ideas, Compete, and Win Big!
           </p>
-          <button className="bg-yellow-400 text-purple-900 font-bold py-3 px-6 md:py-4 md:px-8 rounded-full text-xl md:text-2xl hover:bg-yellow-300 transition duration-300 ease-in-out transform hover:scale-105 animate-pulse">
+          <button className="bg-yellow-400 text-purple-900 font-bold py-2 px-4 md:py-4 md:px-8 rounded-full text-lg md:text-2xl hover:bg-yellow-300 transition duration-300 ease-in-out transform hover:scale-105 animate-pulse">
             Register Now
           </button>
         </section>
 
         <section className="mb-10 -mx-4">
-          {typeof window !== 'undefined' && window.innerWidth > 768 ? (
+          {!isMobile ? (
             <Marquee pauseOnHover>
-              <div className="grid md:grid-cols-3 gap-6 md:gap-8">
+              <div className="flex space-x-4 md:space-x-8">
                 <FeatureCard
-                  icon={<Lightbulb className="w-12 h-12 md:w-16 md:h-16 mb-4 md:mb-6 text-yellow-400" />}
+                  icon={
+                    <Lightbulb className="w-10 h-10 md:w-16 md:h-16 mb-3 md:mb-6 text-yellow-400" />
+                  }
                   title="Innovative Ideas"
                   description="Showcase your groundbreaking concepts and solutions."
                 />
                 <FeatureCard
-                  icon={<Users className="w-12 h-12 md:w-16 md:h-16 mb-4 md:mb-6 text-yellow-400" />}
+                  icon={
+                    <Users className="w-10 h-10 md:w-16 md:h-16 mb-3 md:mb-6 text-yellow-400" />
+                  }
                   title="Team Collaboration"
                   description="Form a team of 2 to 3 members to collaborate and compete."
                 />
                 <FeatureCard
-                  icon={<IndianRupee className="w-12 h-12 md:w-16 md:h-16 mb-4 md:mb-6 text-yellow-400" />}
+                  icon={
+                    <IndianRupee className="w-10 h-10 md:w-16 md:h-16 mb-3 md:mb-6 text-yellow-400" />
+                  }
                   title="Exciting Prizes"
                   description="Win Cash prizes for your innovative solutions."
                 />
               </div>
             </Marquee>
           ) : (
-            <div className="grid gap-6 md:grid-cols-3 md:gap-8">
+            <div className="grid gap-6 w-[80%] mx-auto justify-center">
               <FeatureCard
-                icon={<Lightbulb className="w-12 h-12 md:w-16 md:h-16 mb-4 md:mb-6 text-yellow-400" />}
+                icon={
+                  <Lightbulb className="w-10 h-10 md:w-16 md:h-16 mb-3 md:mb-6 text-yellow-400" />
+                }
                 title="Innovative Ideas"
                 description="Showcase your groundbreaking concepts and solutions."
               />
               <FeatureCard
-                icon={<Users className="w-12 h-12 md:w-16 md:h-16 mb-4 md:mb-6 text-yellow-400" />}
+                icon={
+                  <Users className="w-10 h-10 md:w-16 md:h-16 mb-3 md:mb-6 text-yellow-400" />
+                }
                 title="Team Collaboration"
                 description="Form a team of 2 to 3 members to collaborate and compete."
               />
               <FeatureCard
-                icon={<IndianRupee className="w-12 h-12 md:w-16 md:h-16 mb-4 md:mb-6 text-yellow-400" />}
+                icon={
+                  <IndianRupee className="w-10 h-10 md:w-16 md:h-16 mb-3 md:mb-6 text-yellow-400" />
+                }
                 title="Exciting Prizes"
                 description="Win Cash prizes for your innovative solutions."
               />
@@ -64,19 +93,21 @@ function App() {
           )}
         </section>
 
-        <section className="text-center mb-20 md:mb-32">
-          <h2 className="text-3xl md:text-5xl font-bold mb-6">Event Details</h2>
-          <p className="text-xl md:text-2xl mb-8 md:mb-10">
+        <section className="text-center mb-16 md:mb-32">
+          <h2 className="text-2xl md:text-5xl font-bold mb-4 md:mb-6">
+            Event Details
+          </h2>
+          <p className="text-lg md:text-2xl mb-6 md:mb-10">
             Join us for an exciting day of innovation and creativity!
           </p>
-          <div className="bg-transparent text-2xl md:text-4xl bg-opacity-20 p-6 md:p-8 rounded-lg backdrop-filter backdrop-blur-lg inline-block border border-white">
-            <p className="mb-4">
+          <div className="bg-transparent text-xl md:text-4xl bg-opacity-20 p-4 md:p-8 rounded-lg backdrop-filter backdrop-blur-lg inline-block border border-white">
+            <p className="mb-3 md:mb-4">
               <strong>Date:</strong> August 15, 2024
             </p>
-            <p className="mb-4">
+            <p className="mb-3 md:mb-4">
               <strong>Time:</strong> 9:00 AM - 6:00 PM
             </p>
-            <p className="mb-4">
+            <p className="mb-3 md:mb-4">
               <strong>Venue:</strong> Tech Innovation Center
             </p>
           </div>
@@ -84,17 +115,21 @@ function App() {
 
         <Rules />
         <section className="relative">
-          <TeaRoadmap />
+          {
+            isMobile ? <RoadMapMobile /> : <RoadMap /> 
+          }
         </section>
       </main>
       <div className="text-center py-10 md:py-20 bg-gradient-to-r from-purple-700 to-blue-600">
-        <h2 className="text-3xl md:text-5xl font-bold mb-6">Ready to Innovate?</h2>
+        <h2 className="text-2xl md:text-5xl font-bold mb-4 md:mb-6">
+          Ready to Innovate?
+        </h2>
         <a
           href="#"
-          className="inline-flex items-center bg-yellow-400 text-purple-900 font-bold py-3 px-6 md:py-4 md:px-8 rounded-full text-xl md:text-2xl hover:bg-yellow-300 transition duration-300 ease-in-out transform hover:scale-105"
+          className="inline-flex items-center bg-yellow-400 text-purple-900 font-bold py-2 px-4 md:py-4 md:px-8 rounded-full text-lg md:text-2xl hover:bg-yellow-300 transition duration-300 ease-in-out transform hover:scale-105"
         >
           Apply Now
-          <ArrowRight className="ml-3 w-6 h-6 md:w-8 md:h-8" />
+          <ArrowRight className="ml-2 md:ml-3 w-5 h-5 md:w-8 md:h-8" />
         </a>
       </div>
       <Footer />
@@ -102,12 +137,20 @@ function App() {
   );
 }
 
-function FeatureCard({ icon, title, description }: { icon: JSX.Element; title: string; description: string }) {
+function FeatureCard({
+  icon,
+  title,
+  description,
+}: {
+  icon: JSX.Element;
+  title: string;
+  description: string;
+}) {
   return (
-    <div className="bg-white bg-opacity-20 p-6 md:p-8 rounded-3xl backdrop-filter backdrop-blur-lg">
+    <div className="bg-white bg-opacity-20 p-4 md:p-8 rounded-3xl backdrop-filter backdrop-blur-lg">
       {icon}
-      <h2 className="text-2xl md:text-3xl font-bold mb-4">{title}</h2>
-      <p className="text-lg md:text-xl">{description}</p>
+      <h2 className="text-xl md:text-3xl font-bold mb-2 md:mb-4">{title}</h2>
+      <p className="text-base md:text-xl">{description}</p>
     </div>
   );
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,15 +5,21 @@ export default function Footer() {
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="mb-4 md:mb-0 text-center md:text-left">
             <h3 className="text-xl md:text-2xl font-bold">Ideathon 2024</h3>
-            <p className="mt-2 text-sm md:text-base">Innovate. Collaborate. Win.</p>
+            <p className="mt-2 text-sm md:text-base">
+              Innovate. Collaborate. Win.
+            </p>
           </div>
         </div>
         <div className="mt-6 md:mt-8 border-t border-gray-700 pt-6 md:pt-8 text-center">
-          <p className="text-sm md:text-base">Brought to you by Department of Artificial Intelligence and Department of Computer Science.</p>
-          <p className="text-sm md:text-base mt-2">Brindavan College of Engineering</p>
+          <p className="text-sm md:text-base">
+            Brought to you by Department of Artificial Intelligence and
+            Department of Computer Science.
+          </p>
+          <p className="text-sm md:text-base mt-2">
+            Brindavan College of Engineering
+          </p>
         </div>
       </div>
     </footer>
-  )
+  );
 }
-
